0872161084d115b4c030fbbbd1c5a25a85699467
Shital2005 <shitalvijayprajapati85@gmail.com>
2025-02-28 22:20:43 +0530

    length functions

--- arrays.java
+++ arrays.java
@@ -2,22 +2,36 @@
 // 0 1 2
 // 3 4 5 6
 
-public class arrays {
-    public static void main(String[] args) {
-        int[][] arr = new int[2][];
-        arr[0] = new int[3];
-        arr[1] = new int[4];
-        int count = 0;
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr[i].length; j++) {
-                arr[i][j] = count++;
-            }
-        }
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr[i].length; j++) {
-                System.out.print(arr[i][j] + " ");
-            }
-            System.out.println();
-        }
-    }
-}
+// public class arrays {
+//     public static void main(String[] args) {
+//         int[][] arr = new int[2][];
+//         arr[0] = new int[3];
+//         arr[1] = new int[4];
+//         int count = 0;
+//         for (int i = 0; i < arr.length; i++) {
+//             for (int j = 0; j < arr[i].length; j++) {
+//                 arr[i][j] = count++;
+//             }
+//         }
+//         for (int i = 0; i < arr.length; i++) {
+//             for (int j = 0; j < arr[i].length; j++) {
+//                 System.out.print(arr[i][j] + " ");
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+// public class arrays {
+//     public static void main(String args[]) {
+//        int array[] = {1, 2, 3, 4, 5};
+//        System.out.println("Length of an array is: " + array.length);
+//     }
+//   }
+
+// public class arrays {
+//     public static void main(String args[]) {
+//        String str = "Welcome to InterviewBit";
+//        System.out.println("Length of String using length() method is: " + str.length());
+//     }
+//   }

de0b6ea50ca4c6f0864eb56702d300afb4061ff2
Shital2005 <shitalvijayprajapati85@gmail.com>
2025-03-02 17:12:35 +0530

    largest and smallest

--- arrays.java
+++ arrays.java
@@ -35,3 +35,53 @@
 //        System.out.println("Length of String using length() method is: " + str.length());
 //     }
 //   }
+
+
+//finding target element in an array
+
+
+// import java.util.*;
+// public class arrays{
+//    public static void main(String args[])throws Exception{
+//       Scanner sc = new Scanner(System.in);
+//       int n = sc.nextInt();
+//       int [] arr = new int[n];
+//       for (int i =0;i<n;i++){
+//          arr[i] = sc.nextInt();
+//       }
+//       int target = sc.nextInt();
+//       for(int i=0;i<arr.length;i++){
+//          if (arr[i]== target){
+//             System.out.println(i);
+//             return;
+//          }
+//       }
+//       System.out.println(-1);
+//    }
+// }
+
+//largest and smallest 
+
+
+// import java.util.*;
+// public class arrays{
+//    public static void main(String args[]){
+//       int [] arr = {1,2,3,4,5};
+//       int max = arr[0];
+//       int min = arr[0];
+
+//       for(int number:arr){
+//          if(number>max){
+//             max = number;
+//          }
+//          else if(min>number){
+//             min = number ;
+//          }
+//       }
+//       System.out.println(max);
+//       System.out.println(min);
+//    }
+// }
+
+
+

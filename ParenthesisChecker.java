f5299f68281fff9426440e22a2ef73ef7e86b373
Shital2005 <shitalvijayprajapati85@gmail.com>
2025-03-05 00:52:30 +0530

    Parenthesis_Checker

---
+++ ParenthesisChecker.java
@@ -0,0 +1,43 @@
+import java.util.*;
+public class ParenthesisChecker{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        try{
+        System.out.println("Enter a string:");
+        String str = sc.nextLine();
+        if(isBalanced(str)){
+            System.out.println("Balanced");
+        }
+        else{
+            System.out.println("Not Balanced");
+        }
+        }
+        finally{
+            sc.close();
+        }
+
+    }
+    public static boolean isBalanced(String str){
+        Stack <Character> stack = new Stack<>();
+        for (char ch :str.toCharArray()){
+            switch(ch){
+                case  '(' :
+                case  '{' :
+                case  '[' :
+                    stack.push(ch);
+                    break;
+                case  ')' :
+                    if(stack.isEmpty()||stack.pop()!= '(') return false;
+                    break;
+                case'}':
+                    if(stack.isEmpty() ||stack.pop() != '{') return false;
+                    break;
+                case ']':
+                    if(stack.isEmpty()||stack.pop ()!= '[')return false;
+                    break;
+    }
+}
+    return stack.isEmpty();
+}
+
+}
--- arrays.java
+++ arrays.java
@@ -40,25 +40,29 @@
 //finding target element in an array
 
 
-// import java.util.*;
-// public class arrays{
-//    public static void main(String args[])throws Exception{
-//       Scanner sc = new Scanner(System.in);
-//       int n = sc.nextInt();
-//       int [] arr = new int[n];
-//       for (int i =0;i<n;i++){
-//          arr[i] = sc.nextInt();
-//       }
-//       int target = sc.nextInt();
-//       for(int i=0;i<arr.length;i++){
-//          if (arr[i]== target){
-//             System.out.println(i);
-//             return;
-//          }
-//       }
-//       System.out.println(-1);
-//    }
-// }
+import java.util.*;
+public class arrays{
+   public static void main(String args[])throws Exception{
+      Scanner sc = new Scanner(System.in);
+      try{
+      int n = sc.nextInt();
+      int [] arr = new int[n];
+      for (int i =0;i<n;i++){
+         arr[i] = sc.nextInt();
+      }
+      int target = sc.nextInt();
+      for(int i=0;i<arr.length;i++){
+         if (arr[i]== target){
+            System.out.println(i);
+            return;
+         }
+      }
+      System.out.println(-1);
+    }finally{
+        sc.close();
+    }
+   }
+}
 
 //largest and smallest 
 

b08e2412090eb13e6ca6eb2db5bc33f569d75a52
Shital2005 <shitalvijayprajapati85@gmail.com>
2025-03-08 00:25:46 +0530

    Anagrams

--- anagams.java
+++ anagams.java
@@ -1,3 +1,20 @@
+import java.util.Arrays;
+
 public class anagams {
-    
+   
+    public boolean isAnagram(String s, String t) {
+        if(s.length()!= t.length()){
+            return false;
+        }
+
+        char[] sArray = s.toCharArray();
+        char[] tArray = t.toCharArray();
+
+        Arrays.sort(sArray);
+        Arrays.sort(tArray);
+
+        return Arrays.equals(sArray,tArray);
+
+    }
+
 }

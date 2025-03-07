a9ef71c0a0a61b71b7a8408c14c168ba1d003d48
Shital2005 <shitalvijayprajapati85@gmail.com>
2025-03-07 21:02:23 +0530

    H Index

---
+++ h_index.java
@@ -0,0 +1,26 @@
+public class h_index {
+   
+        public int hIndex(int[] citations) {
+            int n = citations.length;
+            int[] arr = new int[n+1];
+    
+            for(int c:citations){
+                if(c>n){
+                    arr[n]++;   
+                }
+                else{
+                    arr[c]++;
+                }
+            }
+            int count =0;
+            for (int i=n;i>=0;i--){
+                count=count+arr[i];
+                if(count>=i){
+                    return i;
+                }
+               
+            }
+             return 0;
+        }
+    
+}

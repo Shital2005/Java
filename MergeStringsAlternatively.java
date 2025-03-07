15e1c522e3cdbed7e70827d15adfaad92013d1e6
Shital2005 <shitalvijayprajapati85@gmail.com>
2025-03-08 00:53:10 +0530

    Merge Strings Alternatively

---
+++ MergeStringsAlternatively.java
@@ -0,0 +1,17 @@
+public class MergeStringsAlternatively {
+
+        public String mergeAlternately(String word1, String word2) {
+            StringBuilder sb = new StringBuilder();
+            int i = 0,j = 0;
+            int len1 = word1.length(),len2 = word2.length();
+    
+            while(i<len1 || j<len2){
+                if(i<len1)sb.append(word1.charAt(i++));
+                if(j<len2)sb.append(word2.charAt(j++));
+            }
+    
+            return sb.toString();
+    
+        }
+    
+}

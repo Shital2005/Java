6f8dc7b7f2565158cac7ba8e90c5323695536715
Shital2005 <shitalvijayprajapati85@gmail.com>
2025-03-15 15:20:02 +0530

    Updated code

--- InterviewBit.java
+++ InterviewBit.java
@@ -1,19 +1,21 @@
 public class InterviewBit {
-    public static void main(String[] args){
-       int[][] 2dArray = new int[2][];
-      2dArray[0] = new int[3];
-       2dArray[1] = new int[4];
+   public static void main(String[] args) {
+       int[][] array2D = new int[2][];  // Corrected variable name
+       array2D[0] = new int[3];
+       array2D[1] = new int[4];
+
        int counter = 0;
-       for(int row=0; row <2dArray.length; row++){
-          for(int col=0; col < 2dArray[row].length; col++){
-             2dArray[row][col] = counter++;
-          }
+       for (int row = 0; row < array2D.length; row++) {
+           for (int col = 0; col < array2D[row].length; col++) {
+               array2D[row][col] = counter++;
+           }
        }
-       for(int row=0; row < 2dArray.length; row++){
-          System.out.println();
-          for(int col=0; col < 2dArray[row].length; col++){
-             System.out.print(2dnArray[row][col] + " ");
-          }
+
+       for (int row = 0; row < array2D.length; row++) {
+           System.out.println();  // Print new line for each row
+           for (int col = 0; col < array2D[row].length; col++) {
+               System.out.print(array2D[row][col] + " ");  // Fixed variable name
+           }
        }
-    }
-  }
+   }
+}

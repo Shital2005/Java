66772a0aa449d17550ed0cb16e8616617558cd99
Shital2005 <shitalvijayprajapati85@gmail.com>
2025-05-04 13:18:38 +0530

    number of equivalent domino pairs

---
+++ equiDominoes.java
@@ -0,0 +1,18 @@
+public class equiDominoes {
+    public int equidominoes(int[][] dominoes) {
+        Map <Integer, Integer> map = new HashMap<>();
+        int result = 0;
+
+        for(int [] domino : dominoes) {
+            int a = domino[0];
+            int b = domino[1];
+
+            int key = a<b? a*10+b : b*10+a;
+            result += map.getOrDefault(key, 0);
+            map.put(key, map.getOrDefault(key, 0) + 1);
+
+          
+        }
+        return result; 
+    }
+}

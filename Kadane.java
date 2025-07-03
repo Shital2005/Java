c24df93e6933ab320f7efa8866c1e4ca6bb2b0f0
Shital2005 <shitalvijayprajapati85@gmail.com>
2025-07-03 19:12:33 +0530

    Kadane's algorithm

---
+++ Kadane.java
@@ -0,0 +1,15 @@
+public class Kadane {
+    public int maxSubArray(final int[] A) {
+        
+        int maxSoFar = A[0];
+        int currentMax = A[0];
+        
+        for(int i = 1;i<A.length;i++){
+            currentMax = Math.max(A[i],currentMax+A[i]);
+            maxSoFar = Math.max(currentMax,maxSoFar);   
+        }
+        return maxSoFar;
+    }
+}
+
+

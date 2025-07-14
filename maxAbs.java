ce2c502f44d8dc691af2f8e8297cee4f748cdd5c
Shital Prajapati <140049859+Shital2005@users.noreply.github.com>
2025-07-15 01:07:28 +0530

    maximum absolute value

---
+++ maxAbs.java
@@ -0,0 +1,21 @@
+public class Solution {
+    public int maxAbs(int[] A) {
+       int max1 = Integer.MIN_VALUE;
+        int min1 = Integer.MAX_VALUE;
+        int max2 = Integer.MIN_VALUE;
+        int min2 = Integer.MAX_VALUE;
+
+        for (int i = 0; i < A.length; i++) {
+            int p = A[i] + i;
+            int q = A[i] - i;
+
+            max1 = Math.max(max1, p);
+            min1 = Math.min(min1, p);
+
+            max2 = Math.max(max2, q);
+            min2 = Math.min(min2, q);
+        }
+
+        return Math.max(max1 - min1, max2 - min2);
+    }
+}

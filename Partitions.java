a3dfde22d811411f54a144989d7788cca684c615
Shital Prajapati <140049859+Shital2005@users.noreply.github.com>
2025-07-15 23:14:54 +0530

    Partitions having equal sum

---
+++ Partitions.java
@@ -0,0 +1,24 @@
+public class Partitions {   
+    public int solve(int A,int[] B){
+        for(int i = 0;i<A;i++){
+            total += B[i];
+        }
+        int prefixSum = 0;
+        int partSum = total/3;
+        int count = 0;
+        int ways = 0;
+
+        for(int i=0;i<A-1;i++){
+            prefixSum+= B[i];
+
+            if(prefixSum == partSum){
+                count++;
+            }
+            if(prefixSum == 2 * partSum){
+                ways += count;
+            }
+        }
+        return ways;
+
+    }
+}

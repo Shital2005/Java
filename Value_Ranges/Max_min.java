25d1153c98bc931e994f25c5ec7082e69169da9d
Shital Prajapati <140049859+Shital2005@users.noreply.github.com>
2025-07-26 18:49:39 +0530

    updated min max problem

--- Value_Ranges/Max_min.java
+++ Value_Ranges/Max_min.java
@@ -1,49 +1,21 @@
 package Value_Ranges;
+
 public class Solution {
     public int solve(int[] A) {
-        int n = A.length;
-
-        // If only one element, return it twice
-        if (n == 1) {
+        // Handle edge case: single element
+        if (A.length == 1) {
             return 2 * A[0];
         }
 
-        int max, min;
-        int i;
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
 
-        // Initialize min and max
-        if (n % 2 == 0) {
-            if (A[0] > A[1]) {
-                max = A[0];
-                min = A[1];
-            } else {
-                max = A[1];
-                min = A[0];
-            }
-            i = 2;
-        } else {
-            max = min = A[0];
-            i = 1;
+        // Single pass using built-in Math functions
+        for (int num : A) {
+            min = Math.min(min, num);
+            max = Math.max(max, num);
         }
 
-        // Process pairs
-        while (i < n - 1) {
-            int localMax, localMin;
-            if (A[i] > A[i + 1]) {
-                localMax = A[i];
-                localMin = A[i + 1];
-            } else {
-                localMax = A[i + 1];
-                localMin = A[i];
-            }
-
-            if (localMax > max) max = localMax;
-            if (localMin < min) min = localMin;
-
-            i += 2;
-        }
-
-        return max + min;
+        return min + max;
     }
 }
-
